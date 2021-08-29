96fa99c9728b774f761d17ff3a393d7177a23dd2
sw4y74 <85453219+sw4y74@users.noreply.github.com>
2021-08-29 13:53:56 +0200

    baza do sytemu walki

---
+++ Assets/Scripts/Combat/BattleSystem.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleSystem : MonoBehaviour
+{
+
+	public GameObject playerPrefab;
+	public GameObject enemyPrefab;
+
+
+	Unit playerUnit;
+	Unit enemyUnit;
+
+	public Text dialogueText;
+
+	public BattleHUD playerHUD;
+	public BattleHUD enemyHUD;
+
+	public BattleState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		state = BattleState.START;
+		StartCoroutine(SetupBattle());
+    }
+
+	IEnumerator SetupBattle()
+	{
+		GameObject playerGO = Instantiate(playerPrefab);
+		playerUnit = playerGO.GetComponent<Unit>();
+
+		GameObject enemyGO = Instantiate(enemyPrefab);
+		enemyUnit = enemyGO.GetComponent<Unit>();
+
+		dialogueText.text = "Dziki " + enemyUnit.unitName + " wkracza...";
+
+		playerHUD.SetHUD(playerUnit);
+		enemyHUD.SetHUD(enemyUnit);
+
+		yield return new WaitForSeconds(2f);
+
+		state = BattleState.PLAYERTURN;
+		PlayerTurn();
+	}
+
+	IEnumerator PlayerAttack()
+	{
+		bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+
+		enemyHUD.SetHP(enemyUnit.currentHP);
+		dialogueText.text = "Siadło!";
+
+		yield return new WaitForSeconds(2f);
+
+		if(isDead)
+		{
+			state = BattleState.WON;
+			EndBattle();
+		} else
+		{
+			state = BattleState.ENEMYTURN;
+			StartCoroutine(EnemyTurn());
+		}
+	}
+
+	IEnumerator EnemyTurn()
+	{
+		dialogueText.text = enemyUnit.unitName + " atakuje!";
+
+		yield return new WaitForSeconds(1f);
+
+		bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+
+		playerHUD.SetHP(playerUnit.currentHP);
+
+		yield return new WaitForSeconds(1f);
+
+		if(isDead)
+		{
+			state = BattleState.LOST;
+			EndBattle();
+		} else
+		{
+			state = BattleState.PLAYERTURN;
+			PlayerTurn();
+		}
+
+	}
+
+	void EndBattle()
+	{
+		if(state == BattleState.WON)
+		{
+			dialogueText.text = "Wygrałeś bitkę!";
+		} else if (state == BattleState.LOST)
+		{
+			dialogueText.text = "Sroga porażka.";
+		}
+	}
+
+	void PlayerTurn()
+	{
+		dialogueText.text = "Co robimy szefunciu?:";
+	}
+
+	IEnumerator PlayerHeal()
+	{
+		playerUnit.Heal(5);
+
+		playerHUD.SetHP(playerUnit.currentHP);
+		dialogueText.text = "Czujesz się jak młody bóg!";
+
+		yield return new WaitForSeconds(2f);
+
+		state = BattleState.ENEMYTURN;
+		StartCoroutine(EnemyTurn());
+	}
+
+	public void OnAttackButton()
+	{
+		if (state != BattleState.PLAYERTURN)
+			return;
+
+		StartCoroutine(PlayerAttack());
+	}
+
+	public void OnHealButton()
+	{
+		if (state != BattleState.PLAYERTURN)
+			return;
+
+		StartCoroutine(PlayerHeal());
+	}
+
+}
