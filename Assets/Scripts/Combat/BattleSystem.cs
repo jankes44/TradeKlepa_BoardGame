afb9634790cf3534987f388810ed4dd56ccfd1d0
jankes44 <kalima12345@gmail.com>
2021-08-29 22:25:15 +0100

    combat system
    animacje, przerobiony system bazowy, 3 siły ataku, agility strength vitality multipliers, etc.

--- Assets/Scripts/Combat/BattleSystem.cs
+++ Assets/Scripts/Combat/BattleSystem.cs
@@ -10,7 +10,7 @@ public class BattleSystem : MonoBehaviour
 
 	public GameObject playerPrefab;
 	public GameObject enemyPrefab;
-
+	public GameObject combatHUD;
 
 	Unit playerUnit;
 	Unit enemyUnit;
@@ -21,6 +21,8 @@ public class BattleSystem : MonoBehaviour
 	public BattleHUD enemyHUD;
 
 	public BattleState state;
+
+	bool actionInProgress = false;
 
     // Start is called before the first frame update
     void Start()
@@ -48,33 +50,159 @@ public class BattleSystem : MonoBehaviour
 		PlayerTurn();
 	}
 
-	IEnumerator PlayerAttack()
+	IEnumerator PlayerAttack(string typeOfAttack)
 	{
-		bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+		actionInProgress = true;
+		combatHUD.SetActive(false);
+		playerUnit.SlashAnim(true);
+
+		yield return new WaitForSeconds(1f);
+
+		int damage = playerUnit.damage;
+		int range = 100;
+		int chance;
+		float chanceMultiplier = playerUnit.agilityMultiplier;
+		float strengthMultiplier = playerUnit.strengthMultiplier;
+		string attackDialog = "";
+
+		switch (typeOfAttack)
+        {
+			case "strong":
+				chance = Random.Range(1, range);
+
+				//multipliers
+				damage = Mathf.RoundToInt(damage * Random.Range(2.25f, 3f) * strengthMultiplier);
+
+				if (chance < 75 / chanceMultiplier)
+				{
+					damage = 0;
+					attackDialog = "Nie trafiłeś!";
+				} else
+                {
+					attackDialog = "Trafiłeś za "+damage+"!";
+				}
+
+				break;
+			case "medium":
+				chance = Random.Range(1, range);
+				damage = Mathf.RoundToInt(damage * Random.Range(1.25f, 2f) * strengthMultiplier);
+
+				if (chance < 50 / chanceMultiplier)
+				{
+					damage = 0;
+					attackDialog = "Nie trafiłeś!";
+				} else
+                {
+					attackDialog = "Trafiłeś za "+damage+"!";
+				}
+
+				break;
+			case "weak":
+				chance = Random.Range(1, range);
+				damage = Mathf.RoundToInt(damage * Random.Range(0.75f, 1f) * strengthMultiplier);
+
+				if (chance < 15 / chanceMultiplier)
+				{
+					damage = 0;
+					attackDialog = "Nie trafiłeś!";
+				} else
+                {
+					attackDialog = "Trafiłeś za "+damage+"!";
+				}
+
+				break;
+        }
+		bool isDead = enemyUnit.TakeDamage(damage);
 
 		enemyHUD.SetHP(enemyUnit.currentHP);
-		dialogueText.text = "Siadło!";
+		dialogueText.text = attackDialog;
 
 		yield return new WaitForSeconds(2f);
 
-		if(isDead)
+		playerUnit.SlashAnim(false);
+
+		if (isDead)
 		{
 			state = BattleState.WON;
+			actionInProgress = false;
 			EndBattle();
 		} else
 		{
 			state = BattleState.ENEMYTURN;
+			actionInProgress = false;
 			StartCoroutine(EnemyTurn());
 		}
 	}
 
 	IEnumerator EnemyTurn()
 	{
+		int damage = enemyUnit.damage;
+		int range = 100;
+		int chance;
+
+		float agilityMultiplier = enemyUnit.agilityMultiplier;
+		float strengthMultiplier = enemyUnit.strengthMultiplier;
+
+		int typeOfAttack = Random.Range(1, 4);
+		string attackDialog = "";
+
 		dialogueText.text = enemyUnit.unitName + " atakuje!";
 
 		yield return new WaitForSeconds(1f);
 
-		bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+		switch (typeOfAttack)
+		{
+			case 1:
+				chance = Random.Range(1, range);
+				damage = Mathf.RoundToInt(damage * Random.Range(2.25f,3f) * strengthMultiplier);
+
+				if (chance < 75 / agilityMultiplier)
+				{
+					damage = 0;
+					attackDialog = enemyUnit.unitName + " nie trafił!";
+				}
+				else
+				{
+					attackDialog = enemyUnit.unitName + " trafił za " + damage + "!";
+				}
+
+				break;
+			case 2:
+				chance = Random.Range(1, range);
+				damage = Mathf.RoundToInt(damage * Random.Range(1.25f, 2f) * strengthMultiplier);
+
+				if (chance < 50 / agilityMultiplier)
+				{
+					damage = 0;
+					attackDialog = enemyUnit.unitName + " nie trafił!";
+				}
+				else
+				{
+					attackDialog = enemyUnit.unitName + " trafił za " + damage + "!";
+				}
+
+				break;
+			case 3:
+				chance = Random.Range(1, range);
+				damage = Mathf.RoundToInt(damage * Random.Range(0.75f, 1f) * strengthMultiplier);
+
+				if (chance < 15 / agilityMultiplier)
+				{
+					damage = 0;
+					attackDialog = enemyUnit.unitName + " nie trafił!";
+				}
+				else
+				{
+					attackDialog = enemyUnit.unitName + " trafił za " + damage + "!";
+				}
+
+				break;
+		}
+
+		Debug.Log(typeOfAttack);
+
+		bool isDead = playerUnit.TakeDamage(damage);
+		dialogueText.text = attackDialog;
 
 		playerHUD.SetHP(playerUnit.currentHP);
 
@@ -105,33 +233,65 @@ public class BattleSystem : MonoBehaviour
 
 	void PlayerTurn()
 	{
-		dialogueText.text = "Co robimy szefunciu?:";
+		combatHUD.SetActive(true);
+		dialogueText.text = "Co robimy szefunciu?";
 	}
 
 	IEnumerator PlayerHeal()
 	{
+		playerUnit.HealAnim(true);
+		actionInProgress = true;
+		combatHUD.SetActive(false);
+		yield return new WaitForSeconds(1f);
+
 		playerUnit.Heal(5);
 
 		playerHUD.SetHP(playerUnit.currentHP);
-		dialogueText.text = "Czujesz się jak młody bóg!";
 
 		yield return new WaitForSeconds(2f);
+		dialogueText.text = "Czujesz się jak młody bóg!";
+		playerUnit.HealAnim(false);
 
 		state = BattleState.ENEMYTURN;
+		actionInProgress = false;
 		StartCoroutine(EnemyTurn());
 	}
 
-	public void OnAttackButton()
+	public void OnAttackStrongButton()
 	{
 		if (state != BattleState.PLAYERTURN)
 			return;
+		if (actionInProgress)
+			return;
 
-		StartCoroutine(PlayerAttack());
+		StartCoroutine(PlayerAttack("strong"));
+	}
+
+	public void OnAttackMediumButton()
+	{
+		if (state != BattleState.PLAYERTURN)
+			return;
+		if (actionInProgress)
+			return;
+
+		StartCoroutine(PlayerAttack("medium"));
+	}
+
+	public void OnAttackWeakButton()
+	{
+		if (state != BattleState.PLAYERTURN)
+			return;
+		if (actionInProgress)
+			return;
+
+		StartCoroutine(PlayerAttack("weak"));
 	}
 
 	public void OnHealButton()
 	{
 		if (state != BattleState.PLAYERTURN)
+			return;
+		if (actionInProgress)
 			return;
 
 		StartCoroutine(PlayerHeal());
--- Assets/Scripts/Combat/Unit.cs
+++ Assets/Scripts/Combat/Unit.cs
@@ -10,8 +10,50 @@ public class Unit : MonoBehaviour
 
 	public int damage;
 
+	public float agility = 10f;
+	public float strength = 10f;
+	public float vitality = 10f;
+	public float agilityMultiplier;
+	public float strengthMultiplier;
+	public float vitalityMultiplier;
+	public Animator playerAnimator;
+
 	public int maxHP;
 	public int currentHP;
+
+    public void Start()
+    {
+		agilityMultiplier = agility / 100f + 1f;
+		strengthMultiplier = strength / 50f + 1f;
+		vitalityMultiplier = vitality / 100f + 1f;
+		playerAnimator = gameObject.GetComponent<Animator>();
+		Debug.Log(strengthMultiplier);
+	}
+
+	public void Anim(bool set)
+    {
+		Debug.Log(set);
+		int hash = Animator.StringToHash("walk");
+		playerAnimator.SetBool(hash, set);
+	}
+
+	public void HealAnim(bool set)
+    {
+		int hash = Animator.StringToHash("heal");
+		playerAnimator.SetBool(hash, set);
+    }
+
+	public void SlashAnim(bool set)
+	{
+		int hash = Animator.StringToHash("slash");
+		playerAnimator.SetBool(hash, set);
+	}
+
+	public void PunchAnim(bool set)
+	{
+		int hash = Animator.StringToHash("punch");
+		playerAnimator.SetBool(hash, set);
+	}
 
 	public bool TakeDamage(int dmg)
 	{
